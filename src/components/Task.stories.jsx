34a4027539c70d34fa2551c6f92f86a564fdbc84
Linda Aragona <linda.aragona@niche.com>
2025-02-25 12:44:13 -0500

    start changes from ui-testing-handbook visual-testing

--- src/components/Task.stories.jsx
+++ src/components/Task.stories.jsx
@@ -14,6 +14,11 @@ export default {
   tags: ['autodocs'],
   //👇 Our exports that end in "Data" are not stories.
   excludeStories: /.*Data$/,
+  argTypes: {
+    onArchiveTask: { action: 'onArchiveTask' },
+    onTogglePinTask: { action: 'onTogglePinTask' },
+    onEditTitle: { action: 'onEditTitle' },
+  },
   args: {
     ...ActionsData,
   },
@@ -23,25 +28,45 @@ export const Default = {
   args: {
     task: {
       id: '1',
-      title: 'Test Task',
+      title: 'Buy Milk',
       state: 'TASK_INBOX',
     },
   },
 };
 
+// export const Pinned = {
+//   args: {
+//     task: {
+//       ...Default.args.task,
+//       state: 'TASK_PINNED',
+//     },
+//   },
+// };
+
 export const Pinned = {
   args: {
     task: {
-      ...Default.args.task,
+      id: '2',
+      title: 'QA dropdown',
       state: 'TASK_PINNED',
     },
   },
 };
 
+// export const Archived = {
+//   args: {
+//     task: {
+//       ...Default.args.task,
+//       state: 'TASK_ARCHIVED',
+//     },
+//   },
+// };
+
 export const Archived = {
   args: {
     task: {
-      ...Default.args.task,
+      id: '3',
+      title: 'Write schema for account menu',
       state: 'TASK_ARCHIVED',
     },
   },
@@ -49,11 +74,21 @@ export const Archived = {
 
 const longTitleString = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not!`;
 
+// export const LongTitle = {
+//   args: {
+//     task: {
+//       ...Default.args.task,
+//       title: longTitleString,
+//     },
+//   },
+// };
+
 export const LongTitle = {
   args: {
     task: {
-      ...Default.args.task,
+      id: '4',
       title: longTitleString,
+      state: 'TASK_INBOX',
     },
   },
 };
